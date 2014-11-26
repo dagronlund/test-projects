0085be1d92a1af23ae07daf35cf2d85b2192479d
David Gronlund <dagronlund@gmail.com>
2014-11-25 22:40:13 -0700

    Show Ethan how to make classes

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-__author__ = 'David'
+__author__ = 'Fuckers'
 
-print('fuck the world')
+
+class Neuron():
+
+    def __init__(self):
+        self.a = 42
+        print(self.a)
+
+    def think(self):
+        self.a = 43
+        print('I don\' want to think: ' + str(self.a))
+
+
+aNeuron = Neuron()
+aNeuron.think()
