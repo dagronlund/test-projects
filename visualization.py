373be666ca43742f2b3204fe42e9511073f187ab
David Gronlund <dagronlund@gmail.com>
2014-11-27 01:30:45 -0700

    Add visualization

--- visualization.py
+++ visualization.py
@@ -1,18 +1,23 @@
 __author__ = 'David'
 
-import tkinter
-
-top = tkinter.Tk()
-
-C = tkinter.Canvas(top, bg="blue", height=250, width=300)
-
-coord = 10, 50, 240, 210
-arc = C.create_arc(coord, start=0, extent=150, fill="red")
-
-C.pack()
-top.mainloop()
+# import tkinter
+#
+# top = tkinter.Tk()
+#
+# C = tkinter.Canvas(top, bg="blue", height=250, width=300)
+#
+# coord = 10, 50, 240, 210
+# arc = C.create_arc(coord, start=0, extent=150, fill="red")
+#
+# C.pack()
+# top.mainloop()
 
 print('fuck the world')
 
-import vispy
+from vispy import mpl_plot as plt
 
+plt.plot([1, 2, 4, 2, 3], color='r')
+plt.plot([3, 1, 2, 4, 1], color='b')
+# plt.scatter([20, 40], [20, 40])
+
+plt.show(True)
