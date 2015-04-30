4d39e137126e36fb76037cf4070d06fce3f5ef2a
David Gronlund <dagronlund@gmail.com>
2015-04-30 00:19:57 -0600

    Fix adaptive romberg integration, adaptive midpoint bug found.

--- python_testbed/integration.py
+++ python_testbed/integration.py
@@ -15,7 +15,7 @@ def float_range(start, stop, step, inclusive_list=False, include_end=False):
         yield stop
 
 
-def romberg_integral(f, a, b, method, accurate_digits=8, debug=False, adaptive=False):
+def romberg_integral(f, a, b, method, accurate_digits=10, debug=False, adaptive=False):
     results = []
     if debug: print("Romberg Debug\n\t", end=" ")
     for iteration in range(0, math.ceil(accurate_digits / 2)):  # Each iteration gives about two more digits
@@ -25,7 +25,8 @@ def romberg_integral(f, a, b, method, accurate_digits=8, debug=False, adaptive=F
             if sub_iteration == 0:  # First sub-iteration value is the integration
                 steps = math.pow(2, iteration)  # The first iteration is one step, second two steps, fours steps...
                 if adaptive:
-                    epsilon = (iteration + 1) * 2  # Rough estimate at accurate digits required in step
+                    epsilon = math.pow(10.0, -(iteration + 1.0))  # Rough estimate at accurate digits required
+                    if debug: print("Ep: ", epsilon, end=" ", flush=True)
                     sub_results.append(adaptive_definite_integral(f, a, b, epsilon, method))
                 else:
                     sub_results.append(definite_integral(f, a, b, steps, method))
@@ -43,7 +44,7 @@ def romberg_integral(f, a, b, method, accurate_digits=8, debug=False, adaptive=F
 
 def adaptive_definite_integral(f, a, b, epsilon, method):
     area = 0.0
-    mid = (a + b) / 2
+    mid = (a + b) / 2  # Possibly find random mid-point?
     guess = method(f, a, b - a)
     guess_a = method(f, a, mid - a)
     guess_b = method(f, mid, b - mid)
@@ -65,8 +66,11 @@ def definite_integral(f, a, b, steps, method):
 def rect_estimate(f, x, step):
     return f(x + step / 2) * step
 
+trap_counter = 0
 
 def trap_estimate(f, x, step):
+    global trap_counter
+    trap_counter += 1
     return (f(x) + f(x + step)) * step / 2
 
 
@@ -82,32 +86,43 @@ def trig(x):
     return math.sin(x)
 
 
+def run_trap_test(f, a, b):
+    global trap_counter
+    trap_counter = 0
+    print("Trapezoidal: ", definite_integral(f, a, b, 10, trap_estimate), trap_counter)
+    trap_counter = 0
+    print("Adaptive: ", adaptive_definite_integral(f, a, b, .01, trap_estimate), trap_counter)
+    trap_counter = 0
+    print("Romberg: ", romberg_integral(f, a, b, trap_estimate), trap_counter)
+    trap_counter = 0
+    print("Adaptive Romberg: ", romberg_integral(f, a, b, trap_estimate, adaptive=True), trap_counter)
+    print()
+
 print("Actual: ", 0.5)
-#print("Rectangular: ", definite_integral(polynomial, 0.0, 1.0, 10, rect_estimate))
-print("Trapezoidal: ", definite_integral(polynomial, 0.0, 1.0, 10, trap_estimate))
-#print("Quadratic: ", definite_integral(polynomial, 0.0, 1.0, 10, quad_estimate))
-print("Romberg: ", romberg_integral(polynomial, 0.0, 1.0, trap_estimate))
-print()
+run_trap_test(polynomial, 0.0, 1.0)
+# #print("Rectangular: ", definite_integral(polynomial, 0.0, 1.0, 10, rect_estimate))
+# print("Trapezoidal: ", definite_integral(polynomial, 0.0, 1.0, 10, trap_estimate))
+# #print("Quadratic: ", definite_integral(polynomial, 0.0, 1.0, 10, quad_estimate))
+# print("Romberg: ", romberg_integral(polynomial, 0.0, 1.0, trap_estimate))
+# print()
 
 print("Actual: ", 2)
-#print("Rectangular: ", definite_integral(trig, 0.0, math.pi, 20, rect_estimate))
-print("Trapezoidal: ", definite_integral(trig, 0.0, math.pi, 20, trap_estimate))
-#print("Quadratic: ", definite_integral(trig, 0.0, math.pi, 20, quad_estimate))
-print("Romberg: ", romberg_integral(trig, 0.0, math.pi, trap_estimate))
-print()
+run_trap_test(trig, 0.0, math.pi)
+# #print("Rectangular: ", definite_integral(trig, 0.0, math.pi, 20, rect_estimate))
+# print("Trapezoidal: ", definite_integral(trig, 0.0, math.pi, 20, trap_estimate))
+# #print("Quadratic: ", definite_integral(trig, 0.0, math.pi, 20, quad_estimate))
+# print("Romberg: ", romberg_integral(trig, 0.0, math.pi, trap_estimate))
+# print()
 
 print("Actual: ", math.log(2.0) - math.log(1.0))
-print("Romberg: ", romberg_integral(lambda x: 1 / x, 1.0, 2.0, trap_estimate))
-print()
+run_trap_test(lambda x: 1 / x, 1.0, 2.0)
+#print("Romberg: ", romberg_integral(lambda x: 1 / x, 1.0, 2.0, trap_estimate))
+#print()
 
 print("Actual: ", math.sin(4.5 * math.pi) - math.sin(0.0))
-print("Trapezoidal: ", definite_integral(lambda x: math.cos(x), 0.0, 4.5 * math.pi, 10, trap_estimate))
-print("Adaptive: ", adaptive_definite_integral(lambda x: math.cos(x), 0.0, 4.5 * math.pi, .01, trap_estimate))
-print("Romberg: ", romberg_integral(lambda x: math.cos(x), 0.0, 4.5 * math.pi, trap_estimate))
-print("Adaptive Romberg: ", romberg_integral(lambda x: math.cos(x), 0.0, 4.5 * math.pi, trap_estimate, adaptive=True))
-print()
+run_trap_test(lambda x: math.cos(x), 0.0, 4.5 * math.pi)
 
-
+exit()
 
 step = 0.01
 a = 0.0
